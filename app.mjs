957f0714316f09152a0a757b9f8503b0cb7c5aa9
napund4 <napunda@mail.com>
2023-03-04 22:16:38 -0300

    sync

---
+++ app.mjs
@@ -0,0 +1,111 @@
+import products from "./data.mjs";
+
+let cart = [];
+let productosSelect = [];
+
+products.forEach(({ id, title, description, weight, price, img }) => {
+  const productHtml = `<div class=products-card>
+<div class="products-card-description">
+   <h2>${title}</h2>
+    <span class="description">${description}</span>
+    <span class="weight">${weight}</span>
+    <span class="price">R$ ${String(price.toFixed(2)).replace(".", ",")}</span>
+    <button data-idProduct="${id}" class="addCartButton">Adicionar</button>
+</div>
+${img ? `<img src="${img}" alt="${title}">` : ``}
+</div>`;
+  const container = document.querySelector(".products-container");
+  const product = document.createElement("div");
+  product.innerHTML = productHtml;
+  container.appendChild(product);
+});
+
+const buttons = document.querySelectorAll(".addCartButton");
+buttons.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.textContent === "Remover"
+      ? (item.textContent = "Adicionar")
+      : (item.textContent = "Remover");
+    item.classList.toggle("selected");
+    productosSelect = [...productosSelect, Number(item.dataset.idproduct)];
+    addProductsCart(products, Number(item.dataset.idproduct));
+    addProductCartHtml();
+    getCartTotalPrice();
+  });
+});
+
+const addProductsCart = (products, id) => {
+  if (!cart.find((element) => element.id === id)) {
+    const product = products.find((element) => element.id === id);
+    product.amount = 1;
+    cart = [...cart, product];
+  } else {
+    const newCart = cart.filter((element) => element.id != id);
+    cart = newCart;
+  }
+};
+
+//Cart section
+const cartFloat = document.querySelector(".cart-products");
+
+const addProductCartHtml = () => {
+  if (cart.length) {
+    cartFloat.parentNode.classList.remove("hidden");
+  } else {
+    cartFloat.parentNode.classList.add("hidden");
+  }
+  cartFloat.innerHTML = "";
+  cart.forEach((item) => {
+    const itemHtml = document.createElement("div");
+    itemHtml.classList.add("product-cart");
+    itemHtml.innerHTML = `
+    <div class="info-product">
+    <span class="title">${item.title}</span>
+    <span class="price">R$ ${String(item.price.toFixed(2)).replace(
+      ".",
+      ","
+    )}</span>
+    </div>
+    <div>
+    Quantitade: ${item.amount}
+    </div>`;
+    cartFloat.appendChild(itemHtml);
+  });
+};
+
+const getCartTotalPrice = () => {
+  const price = document.querySelector(".send-order .price");
+
+  const totalPrice = cart.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.price,
+    0
+  );
+
+  price.textContent = String(totalPrice.toFixed(2)).replace(".", ",");
+
+  return totalPrice;
+};
+
+const sendOrder = () => {
+  let templateString = `Olá, boa noite. Segue abaixo a lista do meu pedido e demais informações: `;
+  cart.forEach((item) => {
+    templateString += ` Nome de Pedido: ${item.title} `;
+
+    templateString += `Valor: R$ ${String(item.price.toFixed(2)).replace(
+      ".",
+      ","
+    )} `;
+  });
+  templateString += ` *Preço total do pedido: R$ ${String(
+    getCartTotalPrice().toFixed(2)
+  ).replace(".", ",")}*`;
+  window.open(`https://wa.me/+5566996291876?text=${templateString}`);
+};
+
+const sendOrderButton = document.querySelector(".send-order button");
+sendOrderButton.addEventListener("click", sendOrder);
+
+const closeCartBtn = document.querySelector(".cancel-order button");
+closeCartBtn.addEventListener("click", () => {
+  cartFloat.parentNode.classList.add("hidden");
+});
---
+++ data.mjs
@@ -0,0 +1,290 @@
+export default [
+  {
+    id: 1,
+    title: "Cupim Mexicano",
+    description:
+      "400 gramas de tiras de cupim mexicano, acompanhado de tortillas de trigo.\nAcompanhamentos e montagem á sua criatividade!",
+    price: 97.17,
+    weight: "(400g)",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202301172054_3RUK_i.jpg",
+  },
+  {
+    id: 2,
+    title: "Las Carnitas",
+    description:
+      "400 gramas em tiras de lombo de porco, acompanhados de tortillas de trigo.\nAcompanhamentos e montagem à sua criatividade!",
+    price: 82.76,
+    weight: "(400g)",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202301172122_038A_i.jpg",
+  },
+  {
+    id: 3,
+    title: "Burrito Misto",
+    description:
+      "Alface, vinagrete, carne picada, frango picado, chili, e mussarela.\nacompanhamentos: sour cream, guacamole.",
+    price: 37.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205121943_8622_i.jpg",
+  },
+  {
+    id: 4,
+    title: "Burrito com Doritos",
+    description:
+      "Carne picada, Doritos amassados, chilli e mussarela.\nAcompanhamentos: sour cream e quacamole.",
+    price: 38.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202204062247_6SRJ_i.jpg",
+  },
+  {
+    id: 5,
+    title: "Burrito de Carne",
+    description:
+      "Carne picada, alface, vinagrete, chilli, e mussarela.\nacompanhamentos: sour cream, guacamole.",
+    price: 38.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202204062249_13AD_i.jpg",
+  },
+  {
+    id: 6,
+    title: "Burrito de Carne com Azeitona",
+    description:
+      "Carne picada, azeitona, vinagrete, mussarela e chilli.\nacompanhamento: quacamole e sour cream.",
+    price: 38.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122000_FVNQ_i.jpg",
+  },
+  {
+    id: 7,
+    title: "Burrito de Frango",
+    description:
+      "Frango picado, alface, vinagrete, chili, e mussarela.\nacompanhamentos: sour cream, guacamole.",
+    price: 36.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122000_62DD_i.jpg",
+  },
+  {
+    id: 8,
+    title: "Tacos Pancho Villa",
+    description:
+      "Chili, bacon, tomate alface , mussarela.\nacompanhamentos: sour crean guacamole",
+    price: 31.0,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122001_JB0W_i.jpg",
+  },
+  {
+    id: 9,
+    title: "Tacos Pollo Loco",
+    description:
+      "Frango picado, pimentões coloridos refogados, alface, tomate e cheddar.\nacompanhamentos: sour cream, guacamole",
+    price: 27.5,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205121942_08A4_i.jpg",
+  },
+  {
+    id: 10,
+    title: "Tacos de Carne",
+    description:
+      "Carne picada, mussarela, alface. Chili e vinagrete.\nacompanhamentos: sour cream, guacamole.",
+    price: 27.5,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202101271606_nxlB_.jpeg",
+  },
+  {
+    id: 11,
+    title: "Tacos de Frango",
+    description:
+      "Frango picado, vinagrete, alface, chili.\nacompanhamentos: sour cream, guacamole.",
+    price: 27.5,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202101271611_LsM7_.jpeg",
+  },
+  {
+    id: 12,
+    title: "Quesadilla Mista",
+    description:
+      "Vinagrete pimenta verde, frango picado, carne picada e mussarela.\nacompanhamentos: sour cream, guacamole.",
+    price: 48.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122005_UHI8_i.jpg",
+  },
+  {
+    id: 13,
+    title: "Quesadilla com Doritos",
+    description:
+      "Queijo e mussarela, Doritos amassados carne picada, e vinagrete.\nacompanhamentos: quacamole e sour cream",
+    price: 49.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122003_XW55_i.jpg",
+  },
+  {
+    id: 14,
+    title: "Quesadilla de Carne",
+    description:
+      "Vinagrete pimenta verde, carne picada e mussarela.\nacompanhamentos: sour cream e guacamole.",
+    price: 48.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122003_0X53_i.jpg",
+  },
+  {
+    id: 15,
+    title: "Quesadilla de Carne com Azeitona",
+    description:
+      "Queijo e musaarela , azeitona, carne picada e vinagrete.\nacompanhamento: quacamole e sour cream.",
+    price: 49.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122005_7BN7_i.jpg",
+  },
+  {
+    id: 16,
+    title: "Quesadilla de Frango",
+    description:
+      "Vinagrete pimenta verde, frango picado e mussarela.\nacompanhamentos: sour cream, guacamole.",
+    price: 46.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122006_EVE1_i.jpg",
+  },
+  {
+    id: 17,
+    title: "Combo Burritos",
+    description:
+      "01 burrito de carne\n01 burrito misto e 01 burrito de carne com Doritos. Acompanhados com nachos!",
+    price: 11.9,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205061851_AM5E_i.jpg",
+  },
+  {
+    id: 18,
+    title: "Combo Mex",
+    description:
+      "01 quesadilla de carne, 01 burrito misto, 02 tacos de frango e nachos.\nAcompanhamentos: sour cream e guacamole.",
+    price: 11.9,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122008_6262_i.jpg",
+  },
+  {
+    id: 19,
+    title: "Combo Mexican´s",
+    description:
+      "Par de tacos de frango, quesadilla de carne, e Doritos.\nacompanhamentos: sour cream, guacamole.",
+    price: 81.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122009_K302_i.jpg",
+  },
+  {
+    id: 20,
+    title: "Combo Nachos",
+    description:
+      "Doritos. Acompanhamentos: sour cream, guacamole, cheddar e chili.",
+    price: 36.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202205122010_7SPO_i.jpg",
+  },
+  {
+    id: 21,
+    title: "Combo Tacos",
+    description:
+      "Combo com 03 pares de tacos. Sabores mistos, acompanhados de nachos, sour cream e quacamole.",
+    price: 81.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_low/pratos/cb1ab32b-8603-4690-9878-fec2d5f111e3/202301172038_G5GF_i.jpg",
+  },
+  {
+    id: 22,
+    title: "Quesadilla Vegetariano",
+    description:
+      "Queijo com pimentão gratinado, azeitona picada, vinagrete, e Doritos amassados\nacompanhamentos: sour cream e guacamole.",
+    price: 48.99,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_high/webapp/dishes/dish-image-placeholder.png",
+  },
+  {
+    id: 23,
+    title: "Tacos Chesse",
+    description:
+      "Queijo com pimentão gratinado e Doritos triturados\nacompanhamento: sour cream e guacamole.",
+    price: 25.5,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_high/webapp/dishes/dish-image-placeholder.png",
+  },
+  {
+    id: 24,
+    title: "Chilli Bean",
+    description:
+      "Chilli de feijão picante. Feijão vermelho, bacon, calabresa, carne bovina. Apimentado!\nPorção de 230 gramas.",
+    price: 15.0,
+    weight: "(230g)",
+    img: "https://static.ifood-static.com.br/image/upload/t_high/webapp/dishes/dish-image-placeholder.png",
+  },
+  {
+    id: 25,
+    title: "Guacamole",
+    description: "Potinho de guacamole.",
+    price: 6.0,
+    weight: "",
+    img: "",
+  },
+  {
+    id: 26,
+    title: "Molho Cheddar",
+    description: "Potinho de molho cheddar.",
+    price: 6.0,
+    weight: "",
+    img: "",
+  },
+  {
+    id: 27,
+    title: "Molho Chilli",
+    description: "Potinho de molho chilli.",
+    price: 6.0,
+    weight: "",
+    img: "",
+  },
+  {
+    id: 28,
+    title: "Sour Cream",
+    description: "Potinho de sour cream.",
+    price: 6.0,
+    weight: "",
+    img: "",
+  },
+  {
+    id: 29,
+    title: "Creme de Alho",
+    description: "Potinho de Creme de alho!",
+    price: 6.0,
+    weight: "",
+    img: "",
+  },
+  {
+    id: 30,
+    title: "Coca-Cola Original 310ml",
+    description: "Lata 310ml",
+    price: 6.0,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_high/webapp/dishes/dish-image-placeholder.png",
+  },
+  {
+    id: 31,
+    title: "Coca-Cola Light 310ml",
+    description: "Lata 310ml",
+    price: 6.0,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_high/webapp/dishes/dish-image-placeholder.png",
+  },
+  {
+    id: 32,
+    title: "Coca-Cola Original 500ml",
+    description: "Garrafa 500ml",
+    price: 8.0,
+    weight: "",
+    img: "https://static.ifood-static.com.br/image/upload/t_high/webapp/dishes/dish-image-placeholder.png",
+  },
+  {
+    id: 33,
+    title: "Refrigerante Coca Cola Garrafa1,5l",
+    description: "Garrafa 1,5l",
+    price: 12.0,
+    weight: "",
+    img: "",
+  },
+];
